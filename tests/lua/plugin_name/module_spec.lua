9454a5825c271b88111ad6934c8e2dfdb9998c2b
Eric Driussi <ericdriussi@gmail.com>
2022-12-04 10:56:02 +0000

    fix asserts

--- tests/lua/plugin_name/module_spec.lua
+++ tests/lua/plugin_name/module_spec.lua
@@ -2,10 +2,10 @@ local plugin = require("plugin_name.module")
 
 describe("module should", function()
   it("add", function()
-    assert(5, plugin.add(2, 3))
+    assert.equals(5, plugin.add(2, 3))
   end)
 
   it("multiply", function()
-    assert(6, plugin.multiply(2, 3))
+    assert.equals(6, plugin.multiply(2, 3))
   end)
 end)
